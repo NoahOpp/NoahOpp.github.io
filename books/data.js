5b0c33e774e18cff040de8b551093dab7d9f9818
Noah Oppenheim <noah17@bu.edu>
2025-11-05 14:37:40 -0500

    Started actuallly inputing the data in the webiste

--- books/data.js
+++ books/data.js
@@ -2,14 +2,14 @@
 // Minimal data to power the Sivers-style list
 const BOOKS = [
   {
-    title: "The Myth of Sisyphus",
-    author: "Albert Camus",
-    slug: "the-myth-of-sisyphus",      // must match your review filename (without .html)
+    title: "Why we Sleep",
+    author: "Mathew Walker",
+    slug: "Why-We-Sleep",      // must match your review filename (without .html)
     date_read: "2025-10-08",           // YYYY-MM-DD
-    rating: 8,                         // 1–10
-    blurb: "Absurdism 101: life has no inherent meaning; revolt, freedom, and passion are the response.",
-    amazon: ""                         // optional URL
-    // cover: "/books/covers/sisyphus.jpg" // optional local or external image
+    rating: 9,                         // 1–10
+    blurb: "What is happening when we sleep; how to improve sleep; effects on health, learning, memory, longevity.",
+    cover: "/books/covers/sisyphus.jpg",
+    amazon: ""                         // optional
   },
   {
     title: "Thinking, Fast and Slow",
@@ -18,6 +18,7 @@ const BOOKS = [
     date_read: "2025-10-08",
     rating: 8,
     blurb: "Two systems of thought; common cognitive biases; practical ways to think a bit clearer.",
+    cover: "/books/covers/tfas.jpg",
     amazon: ""
   }
 ];
